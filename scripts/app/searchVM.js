18dd83d30fb9df3f7633dce87308e2ae4b9b4a1b
Egor <sapronov.egor@gmail.com>
2014-07-10 15:36:38 +0700

    Update error handler.

--- scripts/app/searchVM.js
+++ scripts/app/searchVM.js
@@ -10,10 +10,8 @@ function searchVM() {
             url: 'http://catalog.api.2gis.ru/search?what=' + self.searchPattern() + '&where=новосибирск&version=1.3&pagesize=10&key=rubmnl3474',
             type: 'GET',
             success: function (items) {
-
-            },
-            error: function (request, status, error) {
-                alert('По запросу ничего не найдено');
+                if (items.total == 0)
+                    alert('По данному запросу ничего не найдено');
             }
         });
     };
