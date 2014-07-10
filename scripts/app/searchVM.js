4e9c806a756a0c75a8a830d27d6dac5b11dba303
Egor <sapronov.egor@gmail.com>
2014-07-10 15:26:44 +0700

    Add view model for search.

---
+++ scripts/app/searchVM.js
@@ -0,0 +1,21 @@
+function searchVM() {
+
+    var self = this;
+
+    self.companies = ko.observableArray([]);
+    self.searchPattern = ko.observable('');
+
+    self.search = function () {
+        $.ajax({
+            url: 'http://catalog.api.2gis.ru/search?what=' + self.searchPattern() + '&where=новосибирск&version=1.3&pagesize=10&key=rubmnl3474',
+            type: 'GET',
+            success: function (items) {
+
+            }
+        });
+    };
+};
+
+$(document).ready(function () {
+    ko.applyBindings(new searchVM());
+});
